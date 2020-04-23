9655155f7a5fb7d7f8b1bd79b89d04d8969dbd60
Elisson Alves Silva <elisson@MacBook-Pro-de-Elisson.local>
2020-04-22 20:22:28 -0400

    first commit

---
+++ handler.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const axios = require('axios');
+
+const hello = async event => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
+
+
+const getAddress = async event => {
+  const { zip } = event.pathParameters;
+  try {
+    const response = await axios(`https://viacep.com.br/ws/${zip}/json/`);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        address: response.data
+      })
+    }
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        error: error.message
+      })
+    }
+  }
+  
+};
+
+module.exports = {
+  hello,
+  getAddress
+};
